901953684566e48d92e07a11c00f1fb634900ac6
Lydia Wagner <lydia.wagner@codeandtheory.com>
2015-12-09 11:39:47 -0500

    starting over

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+
+$( document ).ready(function() {
+
+$('#dp').datepicker();
+
+var country = ['UK', 'US', 'Canada', 'India', 'Malaysia', 'Hong Kong'];
+var organization = ['GPA', 'Enterprise O&T', 'GCB', 'Retail Services', 'GCB O&T','CMPC', 'Enterprise Infrastructure'];
+
+
+});
