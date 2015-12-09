3068afa98e661349ae98ca576a4a108f650ef455
Lydia Wagner <lydia.wagner@codeandtheory.com>
2015-12-09 12:10:53 -0500

    serialize data

--- js/main.js
+++ js/main.js
@@ -2,10 +2,16 @@
 
 $( document ).ready(function() {
 
-$('#dp').datepicker();
+	$('#dp').datepicker();
 
-var country = ['UK', 'US', 'Canada', 'India', 'Malaysia', 'Hong Kong'];
-var organization = ['GPA', 'Enterprise O&T', 'GCB', 'Retail Services', 'GCB O&T','CMPC', 'Enterprise Infrastructure'];
+	var country = ['UK', 'US', 'Canada', 'India', 'Malaysia', 'Hong Kong'];
+	var organization = ['GPA', 'Enterprise O&T', 'GCB', 'Retail Services', 'GCB O&T','CMPC', 'Enterprise Infrastructure'];
+
+	$('.new').on('click', function(){
+		var formNumbers = $( "form" ).serialize();
+		console.log( formNumbers);
+
+	});
 
 
 });
